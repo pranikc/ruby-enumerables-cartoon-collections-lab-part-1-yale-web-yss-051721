2291cdaf93de3e9a20b44df9ba95f503b39cd7db
abstract-instructions-1268 <pranik.chainani@yale.edu>
2021-06-06 20:26:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |element, index|
-    puts "#{index}. #{element+1}"
+    index = index+1
+    puts "#{index}. #{element}"
   end
 end
